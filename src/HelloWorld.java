8d6a85bd3c1ecaaae3f96fe0fe17864f2e084a0e
GramadaF <39902467+GramadaF@users.noreply.github.com>
2018-06-05 09:52:00 +0300

    Update HelloWorld.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
 	public static void main (String[] args) {
 		System.out.println("Hello world");
 		System.out.println("Adding code");
+		System.out.println("code added from Github");
 	}
 }
